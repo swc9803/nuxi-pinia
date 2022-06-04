092eb868c7dcd37034b01f627ac968ad760f6468
swc9803 <swc9803@gmail.com>
2022-06-05 01:54:00 +0900

    nuxt router components test

---
+++ components/Box.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="box" class="box"></div>
+</template>
+
+<script setup>
+import gsap from 'gsap'
+import { ref } from 'vue'
+
+const box = ref()
+
+onMounted(() => {
+  gsap.to(box.value, {
+    xPercent: 200
+  })
+})
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 50px;
+  height: 50px;
+  background: black;
+}
+</style>
---
+++ pages/test/test1.vue
@@ -0,0 +1,9 @@
+<template>
+  test
+  <Box/>
+</template>
+
+<script setup>
+import Box from '@/components/Box.vue'
+
+</script>
