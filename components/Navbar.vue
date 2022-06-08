be29b3931b97ee0c43670f15ca1e523cd70e3069
swc9803 <swc9803@gmail.com>
2022-06-09 01:12:32 +0900

    navbar test

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,45 +1,152 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link
-            v-for="menu in menus" :key="menu.id" :to="{ path:menu.link }">
-            {{ menu.name }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <nav>
+    <ul>
+    <svg class="logo"
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 212 104">
+        <g id="logo">
+          <g id="top">
+            <g id="Rectangle 31" filter="url(#top1_i_45_357)">
+              <path fill="#C4C4C4" d="M13 8.7A10 10 0 0 1 23 0h167c5.1 0 9.4 3.8 10 8.9l5.3 47.4c.7 6-4 11.1-10 11.1H16.9a10 10 0 0 1-10-11.3L13 8.7Z"/>
+            </g>
+            <path id="Rectangle 39" fill="url(#logo1_linear_45_357)" d="M26 13a10 10 0 0 1 9.9-8.7h141c5.1 0 9.4 3.9 10 9l4.3 38.6c.7 6-4 11.2-10 11.2H31A10 10 0 0 1 21 51.8L26 13Z"/>
+            <path id="hole1" fill="#fff" d="M64.2 4.3h16.5v7.6H64.2z"/>
+            <path id="hole2" fill="#fff" d="M131.3 4.3h16.5v7.6h-16.5z"/>
+          </g>
+          <g id="egg">
+            <path id="foundation" fill="#FFE7D1" d="M58.7 67.7c0 17.9-10.5 24.6-23.4 24.6-12.9 0-23.3-6.7-23.3-24.6 0-18 10.4-37.3 23.3-37.3 13 0 23.4 19.3 23.4 37.3Z"/>
+            <path id="Union" fill="#fff" fill-rule="evenodd" d="M42.7 37.7a1 1 0 0 0 .3 1.4c5.4 3.8 7 8.3 8.4 13.3.2.5.9.8 1.3.4.3-.2.4-.5.3-.8-1.3-5.5-2.7-10.5-9-14.5a1 1 0 0 0-1.3.2Zm10.7 20a1.2 1.2 0 1 0 0-2.4 1.2 1.2 0 0 0 0 2.5Z" clip-rule="evenodd"/>
+            <text xml:space="preserve" id="S" fill="#CA3939" font-family="BM JUA_TTF" font-size="20" letter-spacing="0em" style="white-space:pre"><tspan x="29.8" y="56.6">S</tspan></text>
+          </g>
+          <g id="egg_2">
+            <path id="foundation_2" fill="#FFD0A5" d="M129.4 67.7c0 17.9-10.5 24.6-23.4 24.6-13 0-23.4-6.7-23.4-24.6 0-18 10.5-37.3 23.4-37.3 13 0 23.4 19.3 23.4 37.3Z"/>
+            <path id="Union_2" fill="#fff" fill-rule="evenodd" d="M113.4 37.7a1 1 0 0 0 .2 1.4c5.4 3.8 7 8.3 8.5 13.3a.8.8 0 0 0 1.6-.4c-1.3-5.5-2.7-10.5-9-14.5a1 1 0 0 0-1.3.2Zm10.7 20a1.2 1.2 0 1 0 0-2.4 1.2 1.2 0 0 0 0 2.5Z" clip-rule="evenodd"/>
+            <text xml:space="preserve" id="G" fill="#CA3939" font-family="BM JUA_TTF" font-size="20" letter-spacing="0em" style="white-space:pre"><tspan x="100.5" y="56.6">G</tspan></text>
+          </g>
+          <g id="egg_3">
+            <path id="foundation_3" fill="#E3A974" d="M200 67.7c0 17.9-10.4 24.6-23.3 24.6-13 0-23.4-6.7-23.4-24.6 0-18 10.5-37.3 23.4-37.3 12.9 0 23.3 19.3 23.3 37.3Z"/>
+            <path id="Union_3" fill="#fff" fill-rule="evenodd" d="M184 37.7a1 1 0 0 0 .3 1.4c5.4 3.8 7 8.3 8.4 13.3a.8.8 0 0 0 1.6-.4c-1.3-5.5-2.7-10.5-9-14.5a1 1 0 0 0-1.2.2Zm10.7 20a1.2 1.2 0 1 0 0-2.4 1.2 1.2 0 0 0 0 2.5Z" clip-rule="evenodd"/>
+            <text xml:space="preserve" id="G_2" fill="#CA3939" font-family="BM JUA_TTF" font-size="20" letter-spacing="0em" style="white-space:pre"><tspan x="171.1" y="56.6">G</tspan></text>
+          </g>
+          <g id="bottom">
+            <g id="Rectangle 32" filter="url(#bottom1_i_45_357)">
+              <rect width="212" height="9.8" y="64.1" fill="#C4C4C4" rx="4.9"/>
+            </g>
+            <g id="Rectangle 33" filter="url(#bottom2_i_45_357)">
+              <path fill="#C4C4C4" d="M9.1 74.1a1 1 0 0 1 1-1.3h50.5a1 1 0 0 1 1 1.3l-4 12.9-7.3 15.7a1 1 0 0 1-1 .6h-28a1 1 0 0 1-1-.6L13 87 9.1 74Z"/>
+            </g>
+            <g id="Rectangle 34" filter="url(#bottom3_i_45_357)">
+              <path fill="#C4C4C4" d="M79.8 74.1a1 1 0 0 1 1-1.3h50.5a1 1 0 0 1 1 1.3l-4 12.9-7.4 15.7a1 1 0 0 1-.9.6H92a1 1 0 0 1-1-.6L83.8 87l-4-12.9Z"/>
+            </g>
+            <g id="Rectangle 35" filter="url(#bottom4_i_45_357)">
+              <path fill="#C4C4C4" d="M150.4 74.1a1 1 0 0 1 1-1.3h50.5a1 1 0 0 1 1 1.3l-4 12.9-7.3 15.7a1 1 0 0 1-.9.6h-28a1 1 0 0 1-1-.6L154.4 87l-4-12.9Z"/>
+            </g>
+          </g>
+        </g>
+        <defs>
+          <filter id="top1_i_45_357" width="198.6" height="71.4" x="6.8" y="-4" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="-4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="shape" result="effect1_innerShadow_45_357"/>
+          </filter>
+          <filter id="bottom1_i_45_357" width="212" height="11.8" x="0" y="62.1" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="-2"/>
+            <feGaussianBlur stdDeviation="1"/>
+            <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="shape" result="effect1_innerShadow_45_357"/>
+          </filter>
+          <filter id="bottom2_i_45_357" width="52.6" height="33.4" x="9.1" y="69.8" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="-3"/>
+            <feGaussianBlur stdDeviation="1.5"/>
+            <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="shape" result="effect1_innerShadow_45_357"/>
+          </filter>
+          <filter id="bottom3_i_45_357" width="52.6" height="33.4" x="79.7" y="69.8" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="-3"/>
+            <feGaussianBlur stdDeviation="1.5"/>
+            <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="shape" result="effect1_innerShadow_45_357"/>
+          </filter>
+          <filter id="bottom4_i_45_357" width="52.6" height="33.4" x="150.4" y="69.8" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="-3"/>
+            <feGaussianBlur stdDeviation="1.5"/>
+            <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="shape" result="effect1_innerShadow_45_357"/>
+          </filter>
+          <linearGradient id="logo1_linear_45_357" x1="187.5" x2="31" y1="63.1" y2="8.2" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#929292"/>
+            <stop offset="1" stop-color="#929292" stop-opacity=".2"/>
+          </linearGradient>
+        </defs>
+      </svg>
+      <li v-for="menu in menus" :key="menu.id">
+        <nuxt-link :to="{ path:menu.link }">
+          {{ menu.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const menus = ref([
-  { name: 'test1', link: 'test1' },
-  { name: 'test2', link: 'test2' },
-  { name: 'test3', link: 'test3' },
+  { name: '테스트1', link: 'test1' },
+  { name: '테스트2', link: 'test2' },
+  { name: '테스트3', link: 'test3' }
 ])
 
 </script>
 <style lang="scss" scoped>
-// nav {
-//   ul {
-//     display: flex;
-//     gap: 20px;
-//     margin: 0;
-//     padding: 0;
-    // li {
-      
-    // }
-//   }
-// }
+nav {
+  position: relative;
+  transform: translate(-50%); //좌우 상하
+  left: 50%;
+  width: 80%;
+  height: 50px;
+  ul {
+    svg {
+      width: 13%;
+    }
+    display: flex;
+    justify-content: space-around;
+    margin: 0;
+    padding: 0;
+    li {
+      list-style: none;
+      a {
+        line-height: 50px;
+        padding: 20px;
+      }
+    }
+  }
+}
 a.router-link-active {
-  padding-bottom: 5px;
-  border-bottom: 3px solid #cc1111;
+  // padding-bottom: 5px;
+  box-shadow: 0 -5px 0 0 rgb(243, 0, 0) inset; 
 }
 a:link, a:visited, a:hover {
   text-decoration: none;
-  color: green;
+  color: rgb(0, 0, 0);
 }
 </style>
